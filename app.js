892a95321372332c93918b973392c818e85c5aa7
Oleg <marchello3007@gmail.com>
2022-07-30 16:57:19 +0300

    add form

---
+++ app.js
@@ -0,0 +1,31 @@
+// ********** slider ***********
+
+// $(".slider-block").slick();
+
+$(".slider-block").slick({
+  autoplay: true,
+  autoplaySpeed: 3000,
+  dots: true,
+  arrows: false,
+  pauseOnHover: false,
+});
+
+//******   1 :  45 */
+
+// $(".slider-block").slick({
+//   autoplay: true,
+//   autoplaySpeed: 3000,
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   arrows: false,
+//   fade: true,
+//   asNavFor: ".slider-nav",
+// });
+// $(".slider-nav").slick({
+//   slidesToShow: 3,
+//   slidesToScroll: 1,
+//   asNavFor: ".slider-for",
+//   dots: true,
+//   centerMode: true,
+//   focusOnSelect: true,
+// });
